3f3b4a0d400da8bf012033b99e69e36c1b5c6b78
Ben Alexander <005886968@coyote.csusb.edu>
2019-09-25 16:27:14 -0700

    added rollaball tutorial with my own movement scripts
    inside you should find working movement scripts and a 3rd person camera.

---
+++ roll a ball/Assets/Scripts/eyelook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eyelook : MonoBehaviour
+{
+    private Vector2 md;
+
+    private Transform Player;
+
+    void Start()
+    {
+        Player = this.transform.parent.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mc = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        md += mc;
+
+        this.transform.localRotation = Quaternion.AngleAxis(-md.y, Vector3.right);
+
+        Player.localRotation = Quaternion.AngleAxis(md.x, Vector3.up);
+
+    }
+}
