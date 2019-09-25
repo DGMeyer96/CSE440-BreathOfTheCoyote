1c1264d43d09c01b4c3df062fb66f55edff1265a
Daniel Meyer <005051094@coyote.csusb.edu>
2019-09-24 18:52:03 -0700

    Added camera, jump, and movement scripts made by Jose and Ben

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 4f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey("w"))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * moveSpeed * 2.5f;
+        }
+        else if (Input.GetKey("w") && !Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * moveSpeed;
+        }
+        else if (Input.GetKey("s"))
+        {
+            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * moveSpeed;
+        }
+        if (Input.GetKey("a") && !Input.GetKey("d"))
+        {
+            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * moveSpeed;
+        }
+        else if (Input.GetKey("d") && !Input.GetKey("a"))
+        {
+            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * moveSpeed;
+        }
+    }
+}
