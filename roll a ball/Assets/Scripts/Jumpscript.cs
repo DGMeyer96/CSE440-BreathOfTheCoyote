5fef5c20d44873a3db4ecd65fd5fc9fcf3f32bf5
Ben Alexander <005886968@coyote.csusb.edu>
2019-09-26 15:27:27 -0700

    separated scripts

---
+++ roll a ball/Assets/Scripts/Jumpscript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumpscript : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private Collider coll;
+    public float jumpSpeed = 200f;
+    bool pressedJump = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        coll = GetComponent<Collider>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        JumpHandler();
+    }
+
+    void JumpHandler()
+    {
+        float moveJump = Input.GetAxis("Jump");
+        bool isGrounded = CheckGrounded();
+        if (moveJump > 0f)
+        {
+            if (!pressedJump && isGrounded)
+            {
+                pressedJump = true;
+                Vector3 jump = new Vector3(0f, moveJump, 0.0f);
+                rb.AddForce(jump.normalized * jumpSpeed);
+            }
+        }
+        else
+        {
+            pressedJump = false;
+        }
+    }
+
+    bool CheckGrounded()
+    {
+        float sizeX = coll.bounds.size.x;
+        float sizeZ = coll.bounds.size.z;
+        float sizeY = coll.bounds.size.y;
+
+        // Position of the 4 bottom corners of the game object
+        // We add 0.01 in Y so that there is some distance between the point and the floor
+        Vector3 corner1 = transform.position + new Vector3(sizeX / 2, -sizeY / 2 + 0.01f, sizeZ / 2);
+        Vector3 corner2 = transform.position + new Vector3(-sizeX / 2, -sizeY / 2 + 0.01f, sizeZ / 2);
+        Vector3 corner3 = transform.position + new Vector3(sizeX / 2, -sizeY / 2 + 0.01f, -sizeZ / 2);
+        Vector3 corner4 = transform.position + new Vector3(-sizeX / 2, -sizeY / 2 + 0.01f, -sizeZ / 2);
+
+        // Send a short ray down the cube on all 4 corners to detect ground
+        bool grounded1 = Physics.Raycast(corner1, new Vector3(0, -1, 0), 0.01f);
+        bool grounded2 = Physics.Raycast(corner2, new Vector3(0, -1, 0), 0.01f);
+        bool grounded3 = Physics.Raycast(corner3, new Vector3(0, -1, 0), 0.01f);
+        bool grounded4 = Physics.Raycast(corner4, new Vector3(0, -1, 0), 0.01f);
+
+        // If any corner is grounded, the object is grounded
+        return (grounded1 || grounded2 || grounded3 || grounded4);
+    }
+}
--- roll a ball/Assets/Scripts/PlayerController.cs
+++ roll a ball/Assets/Scripts/PlayerController.cs
@@ -4,27 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float speed = 20f;
-    public float jumpSpeed = 200f;
     private Rigidbody rb;
-    private Collider coll;
-    bool pressedJump = false;
+    float speedS;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        coll = GetComponent<Collider>();
     }
 
     void FixedUpdate()
     {
         WalkHandler();
-        JumpHandler();
     }
 
     void WalkHandler()
     {
-        float speedS;
         if (Input.GetKey(KeyCode.LeftShift))
         {
             speedS = speed * 2f;
@@ -35,69 +30,23 @@ public class PlayerController : MonoBehaviour
         }
 
         //two different ways to move cahracter  one uses force another uses transform
-        /*
         //this code has no delay but continues to apply force even after key is released
         //recomend speed 1000
-        float moveVertical = Input.GetAxis("Vertical");
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        movement = movement * speedS * Time.deltaTime;
-        movement = transform.worldToLocalMatrix.inverse * movement;
-        rb.AddForce(transform.position + movement);
-        */
-
-
-
         //this code has a button delay  dont know why.  
         //recomend speed set to 10
+
+        /*
+                Vector3 movementd = new Vector3(moveHorizontal, 0.0f, moveVertical);
+                movementd = movementd * dashSpeed * Time.deltaTime;
+                movementd = transform.worldToLocalMatrix.inverse * movementd;
+                rb.AddForce(transform.position + movementd);
+         */
         float moveVertical = Input.GetAxis("Vertical");
         float moveHorizontal = Input.GetAxis("Horizontal");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         movement = movement.normalized * speedS * Time.deltaTime;
         movement = transform.worldToLocalMatrix.inverse * movement;
         rb.MovePosition(transform.position + movement);
-    }
-
-    void JumpHandler()
-    {
-        float moveJump = Input.GetAxis("Jump");
-        bool isGrounded = CheckGrounded();
-        if (moveJump > 0f)
-        {
-            if (!pressedJump && isGrounded)
-            {
-                pressedJump = true;
-                Vector3 jump = new Vector3(0f, moveJump, 0.0f);
-                rb.AddForce(jump.normalized * jumpSpeed);
-            }
-        }
-        else
-        {
-            pressedJump = false;
-        }
-    }
-
-    bool CheckGrounded()
-    {
-        float sizeX = coll.bounds.size.x;
-        float sizeZ = coll.bounds.size.z;
-        float sizeY = coll.bounds.size.y;
-
-        // Position of the 4 bottom corners of the game object
-        // We add 0.01 in Y so that there is some distance between the point and the floor
-        Vector3 corner1 = transform.position + new Vector3(sizeX / 2, -sizeY / 2 + 0.01f, sizeZ / 2);
-        Vector3 corner2 = transform.position + new Vector3(-sizeX / 2, -sizeY / 2 + 0.01f, sizeZ / 2);
-        Vector3 corner3 = transform.position + new Vector3(sizeX / 2, -sizeY / 2 + 0.01f, -sizeZ / 2);
-        Vector3 corner4 = transform.position + new Vector3(-sizeX / 2, -sizeY / 2 + 0.01f, -sizeZ / 2);
-
-        // Send a short ray down the cube on all 4 corners to detect ground
-        bool grounded1 = Physics.Raycast(corner1, new Vector3(0, -1, 0), 0.01f);
-        bool grounded2 = Physics.Raycast(corner2, new Vector3(0, -1, 0), 0.01f);
-        bool grounded3 = Physics.Raycast(corner3, new Vector3(0, -1, 0), 0.01f);
-        bool grounded4 = Physics.Raycast(corner4, new Vector3(0, -1, 0), 0.01f);
-
-        // If any corner is grounded, the object is grounded
-        return (grounded1 || grounded2 || grounded3 || grounded4);
     }
     private void OnTriggerEnter(Collider other)
     {
