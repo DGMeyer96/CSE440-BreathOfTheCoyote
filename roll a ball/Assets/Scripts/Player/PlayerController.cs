df367fdad343cfc49168c3959f8fbbfd7a82e89c
Ben Alexander <005886968@coyote.csusb.edu>
2019-10-07 15:21:25 -0700

    fixed camera rotation on Yaxis

--- roll a ball/Assets/Scripts/Player/PlayerController.cs
+++ roll a ball/Assets/Scripts/Player/PlayerController.cs
@@ -7,20 +7,35 @@ public class PlayerController : MonoBehaviour
     public float speed = 20f;
     private Rigidbody rb;
     private float speedS;
+    private float old_pos;
+    public bool isfalling;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        old_pos = transform.position.y;
+        isfalling = false;
     }
 
     void FixedUpdate()
     {
         WalkHandler();
+
+        if (old_pos == transform.position.y) //check if palyer is falling or not
+        {
+            isfalling = false;
+        }
+        else
+        {
+            isfalling = true;
+        }
+        old_pos = transform.position.y;
+        Debug.Log(isfalling);
     }
 
     void WalkHandler()
     {
-        if (Input.GetAxis("Sprint") > 0)
+        if (Input.GetAxis("Sprint") > 0 && !isfalling)
         {
 
             speedS = speed * 2f;
