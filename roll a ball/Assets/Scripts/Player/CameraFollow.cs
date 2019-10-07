0c8333be8c4012d5ef696b3176c87b5c7973dc8c
Ben Alexander <005886968@coyote.csusb.edu>
2019-10-07 15:21:25 -0700

    fixed camera rotation on Yaxis

--- roll a ball/Assets/Scripts/Player/CameraFollow.cs
+++ roll a ball/Assets/Scripts/Player/CameraFollow.cs
@@ -4,34 +4,18 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public PlayerController pC;
     public Transform Player;
     private Vector2 md;
-
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
     public Vector3 offset2;
     public float rotatespeed = 5f;
-    private float old_pos;
-    private bool isfalling;
 
-    private void Start()
-    {
-        old_pos = transform.position.y;
-        isfalling = false;
-    }
     void FixedUpdate()
     {
-        if (old_pos == transform.position.y) //check if palyer is falling or not
-        {
-            isfalling = false;
-        }
-        else
-        {
-            isfalling = true;
-        }
-        old_pos = transform.position.y;
 
-        if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0 || Input.GetAxis("RotateCam") > 0 || isfalling)
+        if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0 || Input.GetAxis("RotateCam") > 0 || pC.isfalling)
         {
             
             Vector3 desiredPosition = Player.TransformPoint(offset);//move cam
@@ -41,20 +25,25 @@ public class CameraFollow : MonoBehaviour
             
             Vector2 mc = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));//move players rotation with cam
             md += mc;
-            Player.transform.localRotation = Quaternion.AngleAxis(-md.y, Vector3.right);
+            //Player.transform.localRotation = Quaternion.AngleAxis(-md.y, Vector3.right);
             Player.localRotation = Quaternion.AngleAxis(md.x, Vector3.up);
             
             offset2 = offset;//set nonmoving cam to moving cam start offset
         }
         else
         {
-            Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxisRaw("Mouse X") * rotatespeed, Vector3.up);
+            Vector2 mc = new Vector2(Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X"));//move players rotation with cam
+            Quaternion camTurnAngle = Quaternion.Euler(mc);
+
             offset2 = camTurnAngle * offset2;
+
             Vector3 desiredPosition = Player.TransformPoint(offset2);            //move cam not palyer
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
             transform.position = smoothedPosition;
             transform.LookAt(Player);
 
         }
+        
+
     }
 }
