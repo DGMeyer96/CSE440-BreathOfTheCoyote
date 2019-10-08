1ebb05e7625efaa8a6696a05b31055d2b3617f57
Cameron <cameronmaclean17@gmail.com>
2019-10-08 16:57:52 -0700

    Auto stash before merge of "Cameron_Maclean" and "origin/Cameron_Maclean"
    Daniel told me to commit this

--- Assets/Scripts/Cameron_Scripts/Scripts/Groundslam.cs
+++ Assets/Scripts/Cameron_Scripts/Scripts/Groundslam.cs
@@ -13,11 +13,10 @@ public class Groundslam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
+        
             //Placeholder
             //This program isn't set yet as nothing is prepared to use it
             //If an object that has a certain attachment to it is nearby when it is pressed, then something happens
-        }
+        
     }
 }
