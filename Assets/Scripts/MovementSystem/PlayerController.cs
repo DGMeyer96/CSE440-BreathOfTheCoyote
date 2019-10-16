3ebd1d13699eed7aecc40a92768b5f87db6b3d20
Ben Alexander <005886968@coyote.csusb.edu>
2019-10-16 12:47:02 -0700

    Update PlayerController.cs

--- Assets/Scripts/MovementSystem/PlayerController.cs
+++ Assets/Scripts/MovementSystem/PlayerController.cs
@@ -10,16 +10,33 @@ public class PlayerController : MonoBehaviour
     private float old_pos;
     public bool isfalling;
 
+    private float timerdash;
+    public float timetodash = .4f;//time dash force is applied
+
+    public float cooldowntime = 3f;//time until can dash again
+    private float timer;
+
+    private bool candash;
+    public bool dashing;
+
+    public float dashSpeed = 20f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         old_pos = transform.position.y;
         isfalling = false;
+
+        candash = true;
+        dashing = false;
+        timerdash = 0f;
+        timer = 0f;
     }
 
     void FixedUpdate()
     {
         WalkHandler();
+        DashHandler();
 
         if (old_pos == transform.position.y) //check if palyer is falling or not
         {
@@ -32,7 +49,7 @@ public class PlayerController : MonoBehaviour
         old_pos = transform.position.y;
     }
 
-    void WalkHandler()
+     void WalkHandler()
     {
         if (Input.GetAxis("Sprint") > 0 && !isfalling)
         {
@@ -70,6 +87,48 @@ public class PlayerController : MonoBehaviour
                 }               
             }
 
+        }
+    }
+    private void DashHandler()
+    {
+        //if lCTRL is pressed dashing = true apply force
+        //candash = false start cooldown
+        if (Input.GetButtonDown("Dash") && candash)
+        {
+            candash = false;
+            dashing = true;
+            timer = 0f;
+            timerdash = 0f;
+        }
+
+        if (dashing)//apply dash force for 1 seconds
+        {
+            timerdash += Time.deltaTime;
+            if (timerdash < timetodash)//if timer is less than totaltime then continue applying force
+            {
+                float moveVertical = Input.GetAxis("Vertical");
+                float moveHorizontal = Input.GetAxis("Horizontal");
+                Vector3 movementd = new Vector3(moveHorizontal, 0.0f, moveVertical);
+                movementd = movementd * dashSpeed * Time.deltaTime;
+                movementd = transform.worldToLocalMatrix.inverse * movementd;
+                rb.MovePosition(transform.position + movementd);
+
+            }
+            else if (timerdash >= timetodash)//else stop applying force
+            {
+                timerdash = 0f;
+                dashing = false;
+            }
+        }
+
+        if (!candash)
+        {
+            timer += Time.deltaTime;
+            if (timer >= cooldowntime)
+            {
+                timer = 0;
+                candash = true;
+            }
         }
     }
 }
