6d4563230601cba6de0dea33d824f38ee8acfc9b
Cameron <cameronmaclean17@gmail.com>
2019-10-17 11:57:59 -0700

    Push For backup. HDRP VFX for abilities
    Push in case anything messes up. Only pushes VFX graph I made.

--- Assets/Scripts/Cameron_Scripts/Scripts/Groundslam.cs
+++ Assets/Scripts/Cameron_Scripts/Scripts/Groundslam.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Groundslam : MonoBehaviour
 {
+    public GameObject groundv1;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +14,11 @@ public class Groundslam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            GameObject slamvfx = Instantiate(groundv1, transform.position, transform.rotation);
+
+        }
             //Placeholder
             //This program isn't set yet as nothing is prepared to use it
             //If an object that has a certain attachment to it is nearby when it is pressed, then something happens
