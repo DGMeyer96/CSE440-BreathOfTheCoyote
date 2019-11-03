2c38049bce1fad7e2d88a9ae4c4f519fa072fcf0
Ben Alexander <005886968@coyote.csusb.edu>
2019-11-03 10:13:31 -0800

    1

--- Assets/Scripts/Environment/Elevator.cs
+++ Assets/Scripts/Environment/Elevator.cs
@@ -7,36 +7,21 @@ public class Elevator : MonoBehaviour
     public GameObject[] points;
     int current = 0;
     public float speed;
-    bool canmove;
-    float timer = 0f;
-    float cooldowntime = 2f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (!canmove)
-        {
-            timer += Time.deltaTime;
-            if (timer >= cooldowntime)
-            {
-                timer = 0;
-                canmove = true;
-            }
-        }
 
-
-        if (transform.position != points[current].transform.position && canmove)
+        if (current !=0 && transform.position != points[current].transform.position)
         {
             transform.position = Vector3.MoveTowards(transform.position, points[current].transform.position, Time.deltaTime * speed);
 
         }
-        else
-        {}
+        if (current == 0 && transform.position != points[current].transform.position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, points[current].transform.position, Time.deltaTime * speed);
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -45,7 +30,6 @@ public class Elevator : MonoBehaviour
         {
             current = 1;
             Debug.Log(current);
-            canmove = false;
         }
     }
 
@@ -53,9 +37,6 @@ public class Elevator : MonoBehaviour
     {
         current = 0;
         Debug.Log(current);
-        canmove = false;
-
-
     }
 
 }
