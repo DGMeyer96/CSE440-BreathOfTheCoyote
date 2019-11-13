fc9de04cdf6b74edb77f265397c71d1ce376a29f
Jose Perez <josecsemail@gmail.com>
2019-11-12 17:57:48 -0800

    added some animations and box colliders to the dwarf

---
+++ Assets/MeleeAction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAction : MonoBehaviour
+{
+    Animator meleeAction;
+    private int meleeCounter = 0;
+
+    void Start()
+    {
+        meleeAction = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meleeAction.ResetTrigger("Melee");
+        if (Input.GetMouseButton(0) && meleeCounter == 0)
+        {
+            meleeAction.SetTrigger("Melee");
+            meleeCounter = 1;
+            
+            
+            
+        }
+
+        
+
+
+        
+        
+    }
+}
