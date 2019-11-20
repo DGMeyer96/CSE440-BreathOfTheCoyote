1c5275c5d187c04e4df12be49fd5498053a413b2
Jose Perez <josecsemail@gmail.com>
2019-11-19 19:36:40 -0800

    Melee Animation + Weapon Child

--- Assets/MeleeAction.cs
+++ Assets/MeleeAction.cs
@@ -5,46 +5,80 @@ using UnityEngine;
 public class MeleeAction : MonoBehaviour
 {
     Animator meleeAction;
-    private int weaponCounter = 0;
+    private int attackCounter;
+    private float cooldown;
+    private float reset;
+    
+    
     
 
     void Start()
     {
         meleeAction = gameObject.GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
+
     void Update()
     {
-        if (Input.GetMouseButton(0) && weaponCounter == 0)
+        if(cooldown > 0.5f)
         {
-            meleeAction.SetBool("FirstAttack", true);
-            weaponCounter = 1;
+            if (Input.GetMouseButtonDown(0))
+            {
+                cooldown = 0;
+                Attack();
+            }
         }
-
-        else if(Input.GetMouseButton(0) && weaponCounter == 1)
+        cooldown += Time.deltaTime;
+        if(reset > 2f)
         {
-            meleeAction.SetBool("SecondAttack", true);
-            weaponCounter = 0;
-                
-        }
-        else
-        {
+            reset = 0f;
+            attackCounter = 0;
             meleeAction.SetBool("FirstAttack", false);
             meleeAction.SetBool("SecondAttack", false);
+        }
+
+        reset += Time.deltaTime;
+    }
+
+
+    private void Attack()
+    {
+        if(attackCounter == 0)
+        {
+            
+            meleeAction.SetBool("FirstAttack", true);
+            attackCounter = 1;
+            reset = 0f;
+        }
+        else if (attackCounter == 1)
+        {
+            meleeAction.SetBool("FirstAttack", false);
+            meleeAction.SetBool("SecondAttack", true);
+            attackCounter = 0;
+            reset = 0f;
+        }
+    /*if (Input.GetMouseButtonDown(0) && attackCounter == 0)
+        {
+            meleeAction.SetBool("FirstAttack", true);
+            attackCounter++;
             
         }
 
-        /*else if(Input.GetMouseButtonDown(0) && weaponCounter == 1)
+        else if (Input.GetMouseButtonDown(0) && attackCounter == 1)
         {
             meleeAction.SetBool("SecondAttack", true);
-            weaponCounter = 2;
-        }*/
+            attackCounter++;
+        }
 
-        //if (Input.GetMouseButton(0))
-        //{
-        //    meleeAction.SetBool("FirstAttack", false);
-        //    //meleeAction.SetBool("SecondAttack", false);
-        //}
+        else if (attackCounter == 2)
+        {
+            meleeAction.SetBool("FirstAttack", false);
+            meleeAction.SetBool("SecondAttack", false);
+            meleeAction.SetBool("StopAttack", true);
+            attackCounter = 0;
+        }*/
     }
+
 }
