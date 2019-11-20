2b5fbe5f3c02153b9c6d5dc32830136a3c619209
Daniel Meyer <005051094@coyote.csusb.edu>
2019-11-19 22:32:29 -0800

    Audio change when entering combat

---
+++ Assets/Scripts/Environment/BattleMusicPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleMusicPlayer : MonoBehaviour
+{
+    private int triggerNum = 0;
+    private AudioClip currentClip;
+
+    public AudioClip battleClip;
+    public AudioSource BGMSource;
+
+    private void Start()
+    {
+        currentClip = BGMSource.clip;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "TrialOfStrength" || other.gameObject.tag == "Combat")
+        {
+            BGMSource.clip = battleClip;
+            BGMSource.Play();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "TrialOfStrength" || other.gameObject.tag == "Combat")
+        {
+            BGMSource.Stop();
+            BGMSource.clip = currentClip;
+            BGMSource.Play();
+        }
+    }
+}
